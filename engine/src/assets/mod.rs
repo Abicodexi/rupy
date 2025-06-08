9f90c8d57d039fe52192dff6925a2f77969e6044
sebastian <abismayn@gmail.com>
2025-06-08 16:38:36 +0300

    extend asset service

--- engine/src/assets/mod.rs
+++ engine/src/assets/mod.rs
@@ -4,4 +4,8 @@ pub use loader::*;
 pub mod watcher;
 pub use watcher::*;
 
+pub mod asset_request;
+pub use asset_request::*;
+
 pub mod service;
+pub use service::*;
--- engine/src/assets/service.rs
+++ engine/src/assets/service.rs
@@ -1,23 +1,20 @@
 use crossbeam::channel::{Receiver, Sender};
 use once_cell::sync::OnceCell;
+use pollster::FutureExt;
 use std::sync::Arc;
 use std::sync::RwLock;
 
+use crate::log_info;
+use crate::AssetRequest;
+use crate::CacheStorage;
+use crate::Vertex;
+use crate::VertexInstance;
 use crate::{
     log_error, MaterialManager, ModelManager, PipelineManager, ShaderManager, TextureManager,
 };
 
 static GLOBAL_ASSET_SERVICE: OnceCell<Arc<AssetService>> = OnceCell::new();
 static GLOBAL_ASSET_TX: OnceCell<Sender<AssetRequest>> = OnceCell::new();
-
-pub enum AssetRequest {
-    LoadTexture {
-        texture: String,
-        format: wgpu::TextureFormat,
-    },
-    LoadModel {/* all params */},
-    LoadMaterial {/* all params */},
-}
 
 pub struct AssetService {
     pub queue: Arc<wgpu::Queue>,
@@ -106,17 +103,203 @@ fn spawn_asset_service_thread(
 fn asset_service_thread(service: Arc<AssetService>, rx: Arc<Receiver<AssetRequest>>) {
     while let Ok(request) = rx.recv() {
         match request {
-            AssetRequest::LoadTexture { texture, format } => {
-                let mut textures = service.textures.write().unwrap();
-                if let Err(e) =
-                    textures.get_or_load_texture(&service.queue, &service.device, &texture, format)
-                {
-                    log_error!("{}", e.to_string());
+            AssetRequest::LoadShader { shader } => {
+                if let Ok(mut shaders) = service.shaders.write() {
+                    if let Err(e) = shaders.load(&service.device, &shader) {
+                        log_error!("{}", e.to_string());
+                    }
                 }
             }
-            _ => {}
+            AssetRequest::LoadRenderPipeline {
+                layout,
+                format,
+                key,
+                label,
+            } => {
+                if let Ok(mut pipelines) = service.pipelines.write() {
+                    if !pipelines.render.contains(&key) {
+                        if let Ok(mut shaders) = service.shaders.write() {
+                            if let Ok(pipeline) = create_render_pipeline(
+                                &service.device,
+                                &mut shaders,
+                                layout,
+                                format,
+                                label,
+                            ) {
+                                pipelines.render.insert(key, pipeline.into());
+                            }
+                        }
+                    }
+                }
+            }
+            AssetRequest::LoadTexture { texture } => {
+                if let Ok(mut textures) = service.textures.write() {
+                    if let Err(e) =
+                        pollster::block_on(textures.load(&service.queue, &service.device, &texture))
+                    {
+                        log_error!("{}", e.to_string());
+                    } else {
+                        log_info!("Loaded texture: {}", texture);
+                    }
+                }
+            }
+            AssetRequest::LoadMaterial {
+                bind_group_layouts,
+                mat,
+                v_shader,
+                f_shader,
+                primitive,
+                color_target,
+                depth_stencil,
+            } => {
+                let mat_name = mat.name.clone();
+
+                if let (Ok(mut materials), Ok(mut textures), Ok(mut pipelines), Ok(mut shaders)) = (
+                    service.materials.write(),
+                    service.textures.write(),
+                    service.pipelines.write(),
+                    service.shaders.write(),
+                ) {
+                    let format = color_target.format.clone();
+                    if let Err(e) = materials.load_tobj(
+                        &service.queue,
+                        &service.device,
+                        &mut textures,
+                        &mut shaders,
+                        &mut pipelines,
+                        bind_group_layouts,
+                        mat,
+                        &v_shader,
+                        &f_shader,
+                        primitive,
+                        color_target,
+                        format,
+                        &[Vertex::LAYOUT, VertexInstance::LAYOUT],
+                        depth_stencil,
+                    ) {
+                        log_error!("{}", e.to_string());
+                    } else {
+                        log_info!("Loaded material: {}", mat_name);
+                    }
+                }
+            }
+            AssetRequest::LoadMaterialAsset {
+                bind_group_layouts,
+                asset,
+                format,
+            } => {
+                let asset_name = asset.name.clone();
+
+                if let (Ok(mut materials), Ok(mut textures), Ok(mut pipelines), Ok(mut shaders)) = (
+                    service.materials.write(),
+                    service.textures.write(),
+                    service.pipelines.write(),
+                    service.shaders.write(),
+                ) {
+                    if let Err(e) = materials.load_asset(
+                        &service.queue,
+                        &service.device,
+                        &mut textures,
+                        &mut shaders,
+                        &mut pipelines,
+                        bind_group_layouts,
+                        asset,
+                        format,
+                        &[Vertex::LAYOUT, VertexInstance::LAYOUT],
+                    ) {
+                        log_error!("{}", e.to_string());
+                    } else {
+                        log_info!("Loaded material asset: {}", asset_name);
+                    }
+                }
+            }
+            AssetRequest::LoadModel {
+                file,
+                v_shader,
+                f_shader,
+                bind_group_layouts,
+                primitive,
+                color_target,
+                depth_stencil,
+            } => {
+                if let (
+                    Ok(mut models),
+                    Ok(mut materials),
+                    Ok(mut textures),
+                    Ok(mut pipelines),
+                    Ok(mut shaders),
+                ) = (
+                    service.models.write(),
+                    service.materials.write(),
+                    service.textures.write(),
+                    service.pipelines.write(),
+                    service.shaders.write(),
+                ) {
+                    if let Err(e) = models
+                        .load(
+                            &service.queue,
+                            &service.device,
+                            &mut materials,
+                            &mut textures,
+                            &mut shaders,
+                            &mut pipelines,
+                            &file,
+                            &v_shader,
+                            &f_shader,
+                            &[Vertex::LAYOUT, VertexInstance::LAYOUT],
+                            bind_group_layouts,
+                            color_target.format,
+                            primitive,
+                            color_target,
+                            depth_stencil,
+                        )
+                        .block_on()
+                    {
+                        log_error!("{}", e.to_string());
+                    } else {
+                        log_info!("Loaded model: {}", file);
+                    }
+                }
+            }
+            AssetRequest::LoadModelAsset {
+                bind_group_layouts,
+                format,
+                asset,
+            } => {
+                if let (
+                    Ok(mut models),
+                    Ok(mut materials),
+                    Ok(mut textures),
+                    Ok(mut pipelines),
+                    Ok(mut shaders),
+                ) = (
+                    service.models.write(),
+                    service.materials.write(),
+                    service.textures.write(),
+                    service.pipelines.write(),
+                    service.shaders.write(),
+                ) {
+                    if let Err(e) = models.load_asset(
+                        &service.queue,
+                        &service.device,
+                        &mut materials,
+                        &mut textures,
+                        &mut shaders,
+                        &mut pipelines,
+                        bind_group_layouts,
+                        format,
+                        &[Vertex::LAYOUT, VertexInstance::LAYOUT],
+                        asset,
+                    ) {
+                        log_error!("{}", e.to_string());
+                    }
+                } else {
+                    log_info!("Loaded model asset: {}", asset.name);
+                }
+            }
         }
     }
+    log_info!("Asset service thread exiting");
 }
 
 pub fn asset_service() -> &'static Arc<AssetService> {
